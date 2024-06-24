087b81da73f5901801f8f5b60426057a2e40b528
abnetayele <abnetayele694@gmail.com>
2024-06-24 15:32:14 +0300

    Function for accepting key's and user preference

--- source.py
+++ source.py
@@ -15,3 +15,14 @@ words_to_find = ["yohannes", "abnet", "daniel", "abebaw", "hanna", "kaleb"]
 used_keys = []
 for row in word_grid:
     print(" ".join(row))
+
+clues = 5
+score = 0
+trial = 6
+def printword(clues):
+    for _ in range(trial):
+        puzzleNumber = int(input("\nWrite the key number (1-6): "))
+        if 1 <= puzzleNumber <= 6:
+            wanthint = input("Do you want a clue? (y/n) ").lower()
+            if wanthint == 'y':
+                clues -= 1
